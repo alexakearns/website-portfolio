61561ef3f2f1666e1b3351e6abcbe7a491bc0ca9
Alexa <alexakearns@outlook.com>
2020-08-19 16:26:48 +0100

    Add app.js with text-align. import to index

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Intro from './components/intro'
+
+import './app.css'
+
+export class App extends Component {
+
+  render() {
+    return(
+      <div class="intro-text">
+        <Intro />
+      </div>
+    )
+  }
+
+}
+
+
+export default App;
