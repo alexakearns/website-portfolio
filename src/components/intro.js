1a07cbf1b55250755adbaff0a17cb701267bd695
Alexa <alexakearns@outlook.com>
2020-08-20 16:34:58 +0100

    Add linkedin and github links to intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
+import './intro.css';
+
 export class Intro extends Component {
   render () {
     return (
       <div>
-        <h1>Alexa Kearns</h1>
-        <h3>Full Stack Developer</h3>
+        <div>
+          <h1>Alexa Kearns</h1>
+          <h3>Full Stack Developer</h3>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/alexa-kearns/" className="link">Linked In</a>
+          <a href="https://github.com/alexakearns" className="link">Github</a>
+        </div>
       </div>
     );
   }
