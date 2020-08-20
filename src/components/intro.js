d33c838729dc9710b1888aa54a92fe5a49661e62
Alexa <alexakearns@outlook.com>
2020-08-20 10:35:00 +0100

    Add intro component

---
+++ src/components/intro.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Intro extends Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Alexa Kearns</h1>
+        <h3>Full Stack Developer</h3>
+      </div>
+    )
+  }
+}
+
+export default Intro;
