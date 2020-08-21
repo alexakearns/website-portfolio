af1e6c32a1a7788a27116317ad4073ac911f27eb
Alexa <alexakearns@outlook.com>
2020-08-21 17:31:18 +0100

    Move css for intro text to home.css

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,7 +5,7 @@ import './home.css';
 export class Home extends Component {
   render () {
     return (
-      <div>
+      <div className="intro-text">
         <div>
           <h1>Alexa Kearns</h1>
           <h3>Full Stack Developer</h3>
