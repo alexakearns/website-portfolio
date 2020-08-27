edd212daf0b8fb9281eff1406432de595a3e090f
Alexa <alexakearns@outlook.com>
2020-08-27 18:41:08 +0100

    Create about component

---
+++ src/components/about/about.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './about.css'
+
+export class About extends Component {
+  render () {
+    return (
+      <div className="about_text">
+        <h1>Alexa Kearns</h1>
+        <p>Full stack developer and recent graduate from highly selective Makers Academy software engineering course.</p>
+      </div>
+    );
+  }
+}
+
+export default About;
