1c3920fe1f52e337eb7c914659b282c7ac6fbc9c
Alexa <alexakearns@outlook.com>
2020-08-27 18:40:57 +0100

    Add Navbar to app

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/home/home.js';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
+import Home from './components/home/home.js';
+import About from './components/about/about.js';
 import './app.css';
 
 export class App extends Component {
   render () {
     return (
       <div>
-        <div className="banner">
-        </div>
         <Router>
+          <div className="banner">
+            <Navbar bg="dark">
+              <Nav mr="auto">
+                <Link className='nav-link' to='/'>Home </Link>
+                <Link className='nav-link' to='/about'>About </Link>
+              </Nav>
+            </Navbar>
+          </div>
           <Switch>
             <Route path="/" exact component={ Home } />
+            <Route path="/about" exact component={ About } />
           </Switch>
         </Router>
       </div>
