c932fb367afcc2a8791767dd4381a4ced1102d03
Alexa <alexakearns@outlook.com>
2020-08-28 17:42:06 +0100

    Change class name for title text styling

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -5,7 +5,7 @@ import './about.css';
 export class About extends Component {
   render () {
     return (
-      <div className="about_text">
+      <div className="about-text">
         <h1>Alexa Kearns</h1>
         <p>Full stack developer and recent graduate from highly selective Makers Academy software engineering course.</p>
       </div>
