75b0d75325f652720c88c2e42ee52271cad3f3ff
Alexa <alexakearns@outlook.com>
2020-09-01 17:13:23 +0100

    Add contact link to Navigation

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 import Home from '../home/home';
 import About from '../about/about';
 import Projects from '../projects/projects';
+import Contact from '../contact/contact';
 
 export class Navigation extends Component {
   render () {
     return (
       <div>
-        <Router>
-          <div className="banner">
-            <Navbar bg="dark">
-              <Navbar.Brand id="initials">AK</Navbar.Brand>
-              <Nav mr="auto">
-                <Link className='nav-link link-text' to='/'>Home </Link>
-                <Link className='nav-link link-text' to='/about'>About </Link>
-                <Link className="nav-link link-text" to="/projects">Projects </Link>
-              </Nav>
-            </Navbar>
-          </div>
-          <Switch>
-            <Route path="/" exact component={ Home } />
-            <Route path="/about" exact component={ About } />
-            <Route path="/projects" exact component={ Projects } />
-          </Switch>
-        </Router>
+        <Container className='p-0' fluid={true}>
+          <Router>
+            <div className='banner'>
+              <Navbar inverse fluid bg="dark">
+                <Navbar.Brand id="initials">AK</Navbar.Brand>
+                <Navbar.Collapse id="navbar-toggle" >
+                  <Nav mr="auto">
+                    <Link className='nav-link' to='/'>Home </Link>
+                    <Link className='nav-link' to='/about'>About </Link>
+                    <Link className="nav-link" to="/projects">Projects </Link>
+                    <Link className="nav-link" to="/contact">Contact</Link>
+                  </Nav>
+                </Navbar.Collapse>
+              </Navbar>
+            </div>
+            <Switch>
+              <Route path="/" exact component={ Home } />
+              <Route path="/about" exact component={ About } />
+              <Route path="/projects" exact component={ Projects } />
+              <Route path="/contact" exact component= { Contact } />
+            </Switch>
+          </Router>
+        </Container>
       </div>
     );
   }
