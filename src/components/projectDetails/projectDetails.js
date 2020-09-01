a77d7614305dee9ea871fd6b058f2ccbca2a27f1
Alexa <alexakearns@outlook.com>
2020-09-01 17:59:02 +0100

    Comment out card/button on projDetails

--- src/components/projectDetails/projectDetails.js
+++ src/components/projectDetails/projectDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+// import Button from 'react-bootstrap/Button';
 
 export class ProjectDetails extends Component {
   render () {
