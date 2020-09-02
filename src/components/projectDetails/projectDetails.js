4b81ab8939d08912741c3017bc6f3365e467ea8c
Alexa <alexakearns@outlook.com>
2020-09-02 17:16:35 +0100

    Add second project to project details

--- src/components/projectDetails/projectDetails.js
+++ src/components/projectDetails/projectDetails.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 import './projectDetails.css';
 import MissionCtrl from './images/Mission-ctrl.png';
+import TapedIt from './images/Taped-it.png';
 
 export class ProjectDetails extends Component {
   render () {
@@ -13,10 +14,23 @@ export class ProjectDetails extends Component {
             <div>
               <img src={MissionCtrl} alt="mission ctrl screenshot" className="project-image"></img>
             </div>
-            <h2>Mission Ctrl</h2>
-            <p>Still using your mouse in VS Code? Learning shortcuts the hard way? Mission Ctrl is a fun arcade style game to learn VS Code shortcuts. This group project was built using MERN stack.</p>
-            <button className="deployed-link" href="http://mission-ctrl.surge.sh/">Play Mission Ctrl here</button>
-            <button className="deployed-link" href="https://github.com/hturnbull93/mission-ctrl-api-node">Github Repo</button>
+            <div className="project-text">
+              <h2>Mission Ctrl</h2>
+              <p>Still using your mouse in VS Code? Learning shortcuts the hard way? Mission Ctrl is a fun arcade style game to learn VS Code shortcuts. This group project was built using MERN stack.</p>
+              <button className="deployed-link" href="http://mission-ctrl.surge.sh/">Play Mission Ctrl here</button>
+              <button className="deployed-link" href="https://github.com/hturnbull93/mission-ctrl-api-node">Github Repo</button>
+            </div>
+          </section>
+          <section id="taped-it">
+            <div>
+              <img src={TapedIt} alt="taped it screenshot" className="project-image"></img>
+            </div>
+            <div className="project-text">
+              <h2>Taped It</h2>
+              <p>Slack extension to send voice notes to your colleagues. Group project using Typescript, React, GraphQL, AWS and MongoDB.</p>
+              <button className="deployed-link" href="https://tapedit.netlify.app/">Go to Taped-It</button>
+              <button className="deployed-link" href="https://github.com/voice-notes">Github Repo</button>
+            </div>
           </section>
         </div>
       </div>
