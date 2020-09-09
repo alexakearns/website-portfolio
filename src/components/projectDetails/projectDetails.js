76184f55744cefacd27711d972bb1abf912609b0
Alexa <alexakearns@outlook.com>
2020-09-09 12:25:35 +0100

    Change project links from button to a

--- src/components/projectDetails/projectDetails.js
+++ src/components/projectDetails/projectDetails.js
@@ -17,8 +17,8 @@ export class ProjectDetails extends Component {
             <div className="project-text">
               <h2>Mission Ctrl</h2>
               <p>Still using your mouse in VS Code? Learning shortcuts the hard way? Mission Ctrl is a fun arcade style game to learn VS Code shortcuts. This group project was built using MERN stack.</p>
-              <button className="deployed-link" href="http://mission-ctrl.surge.sh/">Play Mission Ctrl here</button>
-              <button className="deployed-link" href="https://github.com/hturnbull93/mission-ctrl-api-node">Github Repo</button>
+              <a className="deployed-link" href="http://mission-ctrl.surge.sh/">Play Mission Ctrl here</a>
+              <a className="deployed-link" href="https://github.com/hturnbull93/mission-ctrl-api-node">Github Repo</a>
             </div>
           </section>
           <section id="taped-it">
@@ -28,8 +28,8 @@ export class ProjectDetails extends Component {
             <div className="project-text">
               <h2>Taped It</h2>
               <p>Slack extension to send voice notes to your colleagues. Group project using Typescript, React, GraphQL, AWS and MongoDB.</p>
-              <button className="deployed-link" href="https://tapedit.netlify.app/">Go to Taped-It</button>
-              <button className="deployed-link" href="https://github.com/voice-notes">Github Repo</button>
+              <a className="deployed-link" href="https://tapedit.netlify.app/">Go to Taped-It</a>
+              <a className="deployed-link" href="https://github.com/voice-notes">Github Repo</a>
             </div>
           </section>
           <section id="bank-test">
@@ -39,7 +39,7 @@ export class ProjectDetails extends Component {
             <div className="project-text">
               <h2>Banking Tech Test</h2>
               <p>The aim of this project is to produce well tested, easy to read and easy to change code that is produced through a structured process using TDD and good object orientated design.</p>
-              <button className="deployed-link" href="https://github.com/alexakearns/bank_tech_test">Github Repo</button>
+              <a className="deployed-link" href="https://github.com/alexakearns/bank_tech_test">Github Repo</a>
             </div>
           </section>
         </div>
